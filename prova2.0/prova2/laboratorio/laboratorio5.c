91953a89bdac84c6f6585b0fa5001f9a5a79f876
sidts <enricozoratto36@gmail.com>
2024-06-16 21:54:13 -0300

    mudei mas não entra

--- prova2.0/prova2/laboratorio/laboratorio5.c
+++ prova2.0/prova2/laboratorio/laboratorio5.c
@@ -1,99 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct dados_voo
-{
+struct dados_voo {
     int Lenght; // Tempo do voo (Length)
-    int Delay;    // Indicação de atraso (Delay)
+    int Delay;    // Indicação de Delay (Delay)
 };
 
 // Função de particionamento do Quick Sort
-int separa(struct dados_voo v[], int baixo, int alto)
-{
-    int pivo = v[alto].Lenght;
+int separa(struct dados_voo *v[], int baixo, int alto) {
+    int pivo = v[alto]->Lenght;
     int i = (baixo - 1);
 
-    for (int j = baixo; j <= alto - 1; j++)
-    {
-        if (v[j].Lenght < pivo)
-        {
+    for (int j = baixo; j <= alto - 1; j++) {
+        if (v[j]->Lenght < pivo) {
             i++;
             // Troca sem usar função swap
-            struct dados_voo temp = v[i];
+            struct dados_voo *temp = v[i];
             v[i] = v[j];
             v[j] = temp;
         }
     }
     // Troca sem usar função swap
-    struct dados_voo temp = v[i + 1];
+    struct dados_voo *temp = v[i + 1];
     v[i + 1] = v[alto];
     v[alto] = temp;
     return (i + 1);
 }
 
 // Implementação do Quick Sort
-void quicksort(struct dados_voo v[], int baixo, int alto)
-{
-    if (baixo < alto)
-    {
-        int pi = separa(v, baixo, alto);
+void quicksort(struct dados_voo *v[], int baixo, int alto) {
+    if (baixo < alto) {
+        int pi = particionar(v, baixo, alto);
         quicksort(v, baixo, pi - 1);
         quicksort(v, pi + 1, alto);
     }
 }
 
-int main()
-{
+int main() {
     struct dados_voo dados[539384];
+    struct dados_voo *dados_ptrs[539384];
     FILE *fp = fopen("Airlines.csv", "r");
-    if (fp == NULL)
-    {
-        puts("Arquivo não encontrado\n");
-        exit(0);
+    if (fp == NULL) {
+        perror("Erro ao abrir o arquivo de entrada");
+        exit(EXIT_FAILURE);
     }
 
     FILE *ff = fopen("arquivo_ordenado.txt", "w");
-    if (ff == NULL)
-    {
-        puts("Arquivo não criado\n");
-        exit(0);
+    if (ff == NULL) {
+        perror("Erro ao criar o arquivo de saída");
+        fclose(fp);
+        exit(EXIT_FAILURE);
     }
 
     char primeira_linha[256];
-    if (fgets(primeira_linha, sizeof(primeira_linha), fp) == NULL)
-    {
-        puts("Erro ao ler a primeira linha\n");
-        exit(0);
-    }
+    fgets(primeira_linha, sizeof(primeira_linha), fp);
 
     int i = 0;
     char linha[256];
-    while (fgets(linha, sizeof(linha), fp))
-    {
+    while (fgets(linha, sizeof(linha), fp)) {
         if (sscanf(linha, "%*d;%*99[^;];%*d;%*3[^;];%*3[^;];%*d;%*d;%d;%d",
-                   &dados[i].Lenght,
-                   &dados[i].Delay) == 2)
-        {
+                &dados[i].Lenght,
+                &dados[i].Delay) == 2) {
+            dados_ptrs[i] = &dados[i];
             i++;
         }
-        else
-        {
-            printf("Erro ao ler linha: %s\n", linha);
-        }
     }
 
-    printf("Total de registros lidos: %d\n", i);
+    quicksort(dados_ptrs, 0, i - 1);
 
-    quicksort(dados, 0, i - 1);
-
-    for (int j = 0; j < i; j++)
-    {
+    for (int j = 0; j < i; j++) {
         fprintf(ff, "%d;%d\n",
-                dados[j].Lenght,
-                dados[j].Delay);
+                dados_ptrs[j]->Lenght,
+                dados_ptrs[j]->Delay);
     }
-
-    printf("Arquivo de saída criado com sucesso.\n");
 
     fclose(ff);
     fclose(fp);
